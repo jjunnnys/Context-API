4ab25a4c97d8ea9352a74522bed2df9105efc541
ppby <salswnss@gmail.com>
2020-07-14 11:26:17 +0900

    Modify 동적 Context 사용하기/ ColorProvider, ColorConsumer

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,31 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-const ColorContext = createContext({ color: 'black' });
+const ColorContext = createContext({
+  state: { color: 'black', subcolor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  // 기본 값을 맞춰 줌
+  const [color, setColor] = useState('black');
+  const [subcolor, setSubcolor] = useState('red');
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor },
+  };
+
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer 와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
